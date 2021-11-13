74674a059502a0b95ea2e79b19101d5cf3bfedfa
= <=>
2021-11-12 19:47:55 -0500

    Pruebas json en javascript local con fetch

---
+++ clase4/MathTools/frontend/assets/js/areas.js
@@ -0,0 +1,17 @@
+const hacerPeticion = () => {
+    let radio = document.getElementById("r").value;
+    let altura = document.getElementById("h").value;
+    let myModal = new bootstrap.Modal(document.getElementById('exampleModal'))
+    
+    let result = document.getElementById("result");
+
+    if (radio && altura) {
+        fetch(`http://localhost:8080/cilindro?r=${radio}&h=${altura}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                result.innerHTML = parseFloat(data.area).toFixed(2)
+                myModal.show()
+            })
+    }
+}
